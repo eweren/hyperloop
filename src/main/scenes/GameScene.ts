5a3d4448ec4e97be848ba0a10a976cb94d489200
Klaus Reimer <k@ailis.de>
2020-10-03 20:01:23 +0200

    Simplify camera bounds

--- src/main/scenes/GameScene.ts
+++ src/main/scenes/GameScene.ts
@@ -1,6 +1,5 @@
 import { Hyperloop } from "../Hyperloop";
 import { Scene } from "../../engine/scene/Scene";
-import { GAME_HEIGHT, GAME_WIDTH } from "../constants";
 import { PlayerNode } from "../nodes/PlayerNode";
 import { asset } from "../../engine/assets/Assets";
 import { TiledMap } from "../../engine/tiled/TiledMap";
@@ -22,8 +21,10 @@ export class GameScene extends Scene<Hyperloop> {
     }});
 
     public setup() {
-        this.mapNode.moveTo(GAME_WIDTH / 2, GAME_HEIGHT / 2).appendTo(this.rootNode).transform(m => m.scale(1));
+        const map = GameScene.map;
+        this.mapNode.moveTo(0, 0).appendTo(this.rootNode).transform(m => m.scale(1));
         this.camera.setFollow(this.mapNode.getDescendantById("Player"));
-        this.camera.setLimits(new Rect(-330, 0, 1145, 360));
+        this.camera.setLimits(new Rect(0, 0, map.getWidth() * map.getTileWidth(),
+            map.getHeight() * map.getTileHeight()));
     }
 }
