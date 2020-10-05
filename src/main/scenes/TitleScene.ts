58c952dbdaf3acead3b85faa6acff44fe4e4187d
Edu <eduke@gmx.de>
2020-10-05 19:57:42 +0200

    update title scene

--- src/main/scenes/TitleScene.ts
+++ src/main/scenes/TitleScene.ts
@@ -1,8 +1,5 @@
 import { Hyperloop } from "../Hyperloop";
 import { Scene } from "../../engine/scene/Scene";
-import { GAME_HEIGHT, GAME_WIDTH, STANDARD_FONT } from "../constants";
-import { TextNode } from "../../engine/scene/TextNode";
-import { BitmapFont } from "../../engine/assets/BitmapFont";
 import { asset } from "../../engine/assets/Assets";
 import { Direction } from "../../engine/geom/Direction";
 import { ImageNode } from "../../engine/scene/ImageNode";
@@ -14,23 +11,15 @@ import { MusicManager } from "../MusicManager";
 import { FadeTransition } from "../../engine/transitions/FadeTransition";
 
 export class TitleScene extends Scene<Hyperloop> {
-    @asset(STANDARD_FONT)
-    private static font: BitmapFont;
-
     @asset("images/title-image.png")
     private static titleImage: HTMLImageElement;
 
     private imageNode: ImageNode = new ImageNode({ image: TitleScene.titleImage, anchor: Direction.TOP_LEFT});
-    private textNode = new TextNode({ font: TitleScene.font, anchor: Direction.BOTTOM });
 
     public setup() {
         this.inTransition = new FadeTransition();
         this.outTransition = new FadeToBlackTransition({ duration: 0.5, exclusive: true });
         this.imageNode.appendTo(this.rootNode);
-        this.textNode
-            .setText("PRESS ENTER TO START")
-            .moveTo(GAME_WIDTH / 2, GAME_HEIGHT - 64)
-            .appendTo(this.rootNode);
 
         MusicManager.getInstance().loopTrack(0);
     }
