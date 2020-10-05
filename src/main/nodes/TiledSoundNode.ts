08dfaa24a0aecfbdaeb1b0e8e0aa4ffed9cec0e9
Ranjit Mevius <ranjit.mevius@outlook.com>
2020-10-05 16:24:37 +0200

    add loop sounds to TiledSoundNode

--- src/main/nodes/TiledSoundNode.ts
+++ src/main/nodes/TiledSoundNode.ts
@@ -5,10 +5,28 @@ import { TiledSceneArgs } from "../../engine/scene/TiledMapNode";
 import { Hyperloop } from "../Hyperloop";
 
 const soundAssets = [
+    "sounds/loops/loop_breathing.mp3",
+    "sounds/loops/loop_electronicmotor.mp3",
+    "sounds/loops/loop_elektrostatic.mp3",
     "sounds/loops/loop_fan.mp3",
+    "sounds/loops/loop_flamethrower.mp3",
+    "sounds/loops/loop_flies.mp3",
+    "sounds/loops/loop_gas.mp3",
+    "sounds/loops/loop_halogen.mp3",
+    "sounds/loops/loop_occupied.mp3",
+    "sounds/loops/loop_staticRadioSound.mp3"
 ];
 const soundMapping: {[index: string]: number} = {
-    "loop": 0
+    "breathing": 0,
+    "electronicmotor": 1,
+    "elektrostatic": 2,
+    "fan": 3,
+    "flamethrower": 4,
+    "flies": 5,
+    "gas": 6,
+    "halogen": 7,
+    "occupied": 8,
+    "staticRadioSound": 9,
 };
 function getAssetIndexForName(name: string): number {
     return soundMapping[name] ?? -1;
@@ -19,8 +37,8 @@ export class TiledSoundNode extends SoundNode<Hyperloop> {
     private static sounds: Sound[];
 
     public constructor(args?: TiledSceneArgs) {
-        const range = args?.tiledObject?.getOptionalProperty("range", "int")?.getValue() ?? 1;
-        const intensity = args?.tiledObject?.getOptionalProperty("intensity", "int")?.getValue() ?? 100;
+        const range = args?.tiledObject?.getOptionalProperty("range", "int")?.getValue() ?? 10;
+        const intensity = args?.tiledObject?.getOptionalProperty("intensity", "int")?.getValue() ?? 1.0;
         const soundName = args?.tiledObject?.getOptionalProperty("sound", "string")?.getValue() ?? "";
 
         const soundAssetIndex = getAssetIndexForName(soundName);
@@ -28,7 +46,7 @@ export class TiledSoundNode extends SoundNode<Hyperloop> {
         if (soundAssetIndex !== -1) {
             sound = TiledSoundNode.sounds[soundAssetIndex];
         } else {
-            throw new Error(`Sound ${soundName} could not be loaded`);
+            throw new Error(`Sound '${soundName}' could not be loaded`);
         }
 
         super({ ...args , range, intensity, sound});
