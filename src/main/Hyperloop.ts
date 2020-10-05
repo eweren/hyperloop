0c5e21738601abc64b604a5082f8215b70b986fd
Klaus Reimer <k@ailis.de>
2020-10-05 18:31:19 +0200

    Fix order

--- src/main/Hyperloop.ts
+++ src/main/Hyperloop.ts
@@ -316,8 +316,8 @@ export class Hyperloop extends Game {
             pl.moveTo(spawnPoint.x, spawnPoint.y - 10);
             this.getCamera().setFollow(pl);
             const root = this.getGameScene().rootNode;
-            root.appendChild(pl);
             oldPlayer.remove();
+            root.appendChild(pl);
             // TODO leave remains of old player
         } else {
             // Game Over or sequence of new train replacing old one
