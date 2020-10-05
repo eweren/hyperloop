89c02fffc7e14c5406aa3c49b7a1a47c47c1e421
Ranjit Mevius <ranjit.mevius@outlook.com>
2020-10-05 13:39:36 +0200

    more randomness for mice/rats!

--- src/main/nodes/RatNode.ts
+++ src/main/nodes/RatNode.ts
@@ -6,6 +6,7 @@ import { SceneNodeArgs } from "../../engine/scene/SceneNode";
 import { Sound } from "../../engine/assets/Sound";
 import { asset } from "../../engine/assets/Assets";
 import { Rect } from "../../engine/geom/Rect";
+import { rnd } from "../../engine/util/random";
 
 export class RatNode extends EnemyNode {
     @asset("sprites/mouse.aseprite.json")
@@ -20,7 +21,7 @@ export class RatNode extends EnemyNode {
     protected targetPosition: ReadonlyVector2;
 
     /** minimum distance between enemy and player to stop escaping */
-    private squaredSafetyDistance = 50 ** 2;
+    private squaredSafetyDistance = 100 ** 2;
 
     public constructor(args?: SceneNodeArgs) {
         super({
@@ -30,9 +31,14 @@ export class RatNode extends EnemyNode {
             sourceBounds: new Rect(3, 6, 11, 6),
             ...args
         });
+        this.updateMoveAroundAnchor();
+        this.setState(AiState.MOVE_AROUND);
         this.targetPosition = this.getPosition();
-        this.moveAroundAfterChase = true;
         this.hitpoints = 1;
+    }
+
+    public updateMoveAroundAnchor(position = this.getPosition()): void {
+        this.moveAroundAnchor.setVector(position);
     }
 
     protected updateAi(dt: number, time: number) {
@@ -43,7 +49,6 @@ export class RatNode extends EnemyNode {
         }
         // AI
         switch (this.state) {
-            case AiState.BORED:
             case AiState.ALERT:
                 this.updateAlert(time);
                 break;
@@ -53,40 +58,44 @@ export class RatNode extends EnemyNode {
         }
     }
 
+    private moveTimeMin = 0.3;
+    private moveTimeMax = 1;
+    private waitTimeMin = 2;
+    private waitTimeMax = 5;
+    private moveDelay = rnd(this.moveTimeMin, this.moveTimeMax);
+    private waitTime = rnd(this.waitTimeMin, this.waitTimeMax);
+
     protected updateMoveAround(time: number): void {
-        if (this.getPosition().getSquareDistance(this.moveAroundAnchor) > this.squaredMoveAroundDistance) {
-            if (this.stopAndWaitTs === 0) {
-                if (this.moveTs + this.moveDelaySec < time) {
-                    this.setTag("idle");
-                    this.setDirection(0);
-                    this.stopAndWaitTs = time;
-                }
-            } else if (this.stopAndWaitTs + this.stopAndWaitDelaySec < time) {
-                this.setTag("walk");
-                if (this.getX() > this.moveAroundAnchor.x) {
-                    this.setDirection(-1);
-                } else {
-                    this.setDirection(1);
-                }
-                this.stopAndWaitTs = 0;
-                this.moveTs = time;
+        if (this.stopAndWaitTs === 0) {
+            if (this.moveTs + this.moveDelay < time) {
+                this.setDirection(0);
+                this.stopAndWaitTs = time;
+                this.waitTime = rnd(this.waitTimeMin, this.waitTimeMax);
             }
+        } else if (this.stopAndWaitTs + this.waitTime < time) {
+            this.setDirection(this.getX() >= this.moveAroundAnchor.x ? -1 : 1);
+            this.stopAndWaitTs = 0;
+            this.moveTs = time;
+            this.moveDelay = rnd(this.moveTimeMin, this.moveTimeMax);
         }
         if (this.getDistanceToPlayerSquared() < this.squaredSafetyDistance) {
+            this.escapeDistanceSquared = rnd(this.escapeDistanceMin, this.escapeDistanceMax) ** 2;
             this.setState(AiState.ALERT);
             this.stopSounds();
             RatNode.ratSoundAttack.play();
         }
     }
 
+    private escapeDistanceMin = 150;
+    private escapeDistanceMax = 250;
+    private escapeDistanceSquared = rnd(this.escapeDistanceMin, this.escapeDistanceMax) ** 2;
+
     private updateAlert(time: number): void {
         const player = this.getPlayer();
-        if (player && this.getDistanceToPlayerSquared() < this.squaredSafetyDistance) {
-            this.setTag("walk");
+        if (player && this.getDistanceToPlayerSquared() < this.squaredSafetyDistance + this.escapeDistanceSquared) {
             this.setDirection(this.getX() < player.getX() ? -1 : 1);
         } else {
-            this.targetPosition = this.getPosition();
-            this.moveAroundAnchor.setVector(this.targetPosition);
+            this.updateMoveAroundAnchor();
             this.setState(AiState.MOVE_AROUND);
         }
     }
@@ -109,5 +118,4 @@ export class RatNode extends EnemyNode {
     private isSoundPlaying() {
         return RatNode.ratSoundAttack.isPlaying() || RatNode.ratSoundFollow.isPlaying();
     }
-
 }
