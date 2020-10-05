cd3a2bcda34e2da44a1075617c1928dbf297e87f
Klaus Reimer <k@ailis.de>
2020-10-05 21:23:44 +0200

    No interaction with deadspace suite

--- src/main/nodes/DeadSpaceSuiteNode.ts
+++ src/main/nodes/DeadSpaceSuiteNode.ts
@@ -1,17 +1,11 @@
 import { Aseprite } from "../../engine/assets/Aseprite";
 import { Direction } from "../../engine/geom/Direction";
-import { InteractiveNode } from "./InteractiveNode";
 import { asset } from "../../engine/assets/Assets";
 import { SceneNodeArgs } from "../../engine/scene/SceneNode";
-import { rndItem } from "../../engine/util/random";
+import { AsepriteNode } from "../../engine/scene/AsepriteNode";
+import { Hyperloop } from "../Hyperloop";
 
-const reactions: [ string, number ][] = [
-    [ "That's why she's taking so long...", 3 ],
-    [ "Thank God. That guy was annoying", 3 ],
-    [ "Ugh", 2 ]
-];
-
-export class DeadSpaceSuitNode extends InteractiveNode {
+export class DeadSpaceSuitNode extends AsepriteNode<Hyperloop> {
     @asset("sprites/deadspacesuit.aseprite.json")
     private static readonly sprite: Aseprite;
 
@@ -19,13 +13,8 @@ export class DeadSpaceSuitNode extends InteractiveNode {
         super({
             aseprite: DeadSpaceSuitNode.sprite,
             anchor: Direction.BOTTOM,
-            tag: "off",
+            tag: "idle",
             ...args
         });
-    }
-
-    public interact(): void {
-        const player = this.getTarget();
-        player?.say(...rndItem(reactions));
     }
 }
