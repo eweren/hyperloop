9fef6804bf81e20e29ed0ca81e02304af1f0549d
m.over <m.over@iplabs.de>
2020-10-05 15:21:14 +0200

    Fix particles, sort of

---
+++ src/main/nodes/MarkNode.ts
@@ -0,0 +1,36 @@
+import { SceneNode, SceneNodeArgs } from "../../engine/scene/SceneNode";
+import { Layer } from "../constants";
+import { Hyperloop } from "../Hyperloop";
+
+
+export class MarkNode extends SceneNode<Hyperloop> {
+    private startTime = 0;
+    private killTime = 0;
+
+    public constructor(args: SceneNodeArgs) {
+        super({
+            width: 1,
+            height: 1,
+            layer: Layer.FOREGROUND,
+            ... args
+        });
+    }
+
+    public update(dt: number, time: number): void {
+        if (this.startTime === 0) {
+            this.startTime = time;
+            this.killTime = this.startTime + 5000;
+        } else if (time > this.killTime) {
+            super.remove();
+        }
+    }
+
+    public draw(context: CanvasRenderingContext2D): void {
+        context.strokeStyle = "green";
+        context.beginPath();
+        context.moveTo(0, 3);
+        context.arc(0, 0, 3, 0, 6.28);
+        context.closePath();
+        context.stroke();
+    }
+}
