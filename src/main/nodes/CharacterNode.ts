da5036cd7e261c6d595beff4383125d481d78fed
m.over <m.over@iplabs.de>
2020-10-05 11:48:52 +0200

    Add blood particles when shot at

--- src/main/nodes/CharacterNode.ts
+++ src/main/nodes/CharacterNode.ts
@@ -5,9 +5,11 @@ import { ReadonlyVector2, Vector2, Vector2Like } from "../../engine/graphics/Vec
 import { AsepriteNode, AsepriteNodeArgs } from "../../engine/scene/AsepriteNode";
 import { cacheResult } from "../../engine/util/cache";
 import { clamp } from "../../engine/util/math";
+import { rnd } from "../../engine/util/random";
 import { Hyperloop } from "../Hyperloop";
 import { CollisionNode } from "./CollisionNode";
 import { InteractiveNode } from "./InteractiveNode";
+import { ParticleNode, valueCurves } from "./ParticleNode";
 import { PlayerArmNode } from "./player/PlayerArmNode";
 import { PlayerLegsNode } from "./player/PlayerLegsNode";
 
@@ -48,10 +50,31 @@ export abstract class CharacterNode extends AsepriteNode<Hyperloop> {
     private bulletEndPoint: Vector2 | null = null;
     private storedCollisionCoordinate: Vector2 = new Vector2(0, 0);
 
+    protected bloodEmitter: ParticleNode;
+    private bloodOffset: Vector2 = new Vector2(0, 0);
+    private bloodAngle = 0;
+
     public constructor(args: AsepriteNodeArgs) {
         super(args);
         this.velocity = new Vector2(0, 0);
         // this.setShowBounds(true);
+
+        this.bloodEmitter = new ParticleNode({
+            offset: () => this.bloodOffset,
+            velocity: () => {
+                const speed = rnd(20, 50);
+                const angle = this.bloodAngle + rnd(-1, 1) * rnd(0, Math.PI / 2) ** 2;
+                return {
+                    x: speed * Math.cos(angle),
+                    y: speed * Math.sin(angle)
+                };
+            },
+            color: () => `rgb(${rnd(100,240)}, ${rnd(0, 30)}, 0)`,
+            size: rnd(1, 3),
+            gravity: {x: 0, y: -100},
+            lifetime: () => rnd(0.5, 1),
+            alphaCurve: valueCurves.trapeze(0.05, 0.2)
+        }).appendTo(this);
     }
 
     public update(dt: number, time: number): void {
@@ -156,8 +179,18 @@ export abstract class CharacterNode extends AsepriteNode<Hyperloop> {
                 const headshot = (coord.y < bounds.minY + 0.25 * (bounds.height));
                 const damage = headshot ? (2.4 * power) : power;
                 isColliding.hurt(damage, this.getScenePosition());
+                // Blood particles at hurt character
+                isColliding.emitBlood(coord.x, coord.y, angle, headshot ? 30 : 10);
             }
         }
+    }
+
+    public emitBlood(x: number, y: number, angle: number, count = 1): void {
+        const pos = this.getScenePosition();
+        this.bloodOffset = new Vector2(x - pos.x, y - pos.y);
+        this.bloodAngle = angle;
+        console.log(angle);
+        this.bloodEmitter.emit(count);
     }
 
     protected getLineCollision(x1: number, y1: number, dx: number, dy: number, stepSize = 5): CharacterNode | CollisionNode | null {
--- src/main/scenes/GameScene.ts
+++ src/main/scenes/GameScene.ts
@@ -20,6 +20,7 @@ import { MonsterNode } from "../nodes/MonsterNode";
 import { RatNode } from "../nodes/RatNode";
 import { CorpseNode } from "../nodes/CorpseNode";
 import { FuseboxNode } from "../nodes/FuseboxNode";
+import { EnemyNode } from '../nodes/EnemyNode';
 
 export class GameScene extends Scene<Hyperloop> {
     @asset(STANDARD_FONT)
@@ -55,6 +56,10 @@ export class GameScene extends Scene<Hyperloop> {
         // door.moveTo(1040, 380).setLocked(true).appendTo(this.mapNode);
         // new SwitchNode({ onlyOnce: false, onUpdate: (state) => door.setLocked(!state) }).moveTo(1130, 380).appendTo(this.mapNode);
         new SwitchNode({ onlyOnce: true }).moveTo(250, 380).appendTo(this.mapNode);
+        // Test enemies
+        new MonsterNode().moveTo(2400, 360).appendTo(this.mapNode);
+        new MonsterNode().moveTo(2500, 360).appendTo(this.mapNode);
+        new MonsterNode().moveTo(2800, 360).appendTo(this.mapNode);
 
         if (isDev()) {
             this.rootNode.appendChild(new FpsCounterNode({
