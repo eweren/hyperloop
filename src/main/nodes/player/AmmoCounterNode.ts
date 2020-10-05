9dfbd7149738bee073020f0467853a47a2fb4861
m2u-84 <11140006+m2u-84@users.noreply.github.com>
2020-10-05 13:10:27 +0200

    Adjust ammo HUD text

--- src/main/nodes/player/AmmoCounterNode.ts
+++ src/main/nodes/player/AmmoCounterNode.ts
@@ -5,6 +5,6 @@ export class AmmoCounterNode extends TextNode<Hyperloop> {
 
     public update(dt: number, time: number) {
         super.update(dt, time);
-        this.setText(`${this.getGame().getPlayer().getAmmo()} I ${this.getGame().getPlayer().getMagazineSize()}`);
+        this.setText(`${this.getGame().getPlayer().getAmmo()}/${this.getGame().getPlayer().getMagazineSize()}`);
     }
 }
